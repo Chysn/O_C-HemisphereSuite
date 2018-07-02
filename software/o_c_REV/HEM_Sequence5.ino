4d09ca60f30264e4d6442727bfcfd788d31730e8
Jason Justian <jjustian@gmail.com>
2018-07-02 03:29:11 -0400

    Add semitone quantization and saving to Sequence5

--- software/o_c_REV/HEM_Sequence5.ino
+++ software/o_c_REV/HEM_Sequence5.ino
@@ -1,3 +1,7 @@
+#include "braids_quantizer.h"
+#include "braids_quantizer_scales.h"
+#include "OC_scales.h"
+
 class Sequence5 : public HemisphereApplet {
 public:
 
@@ -6,7 +10,9 @@ public:
     }
 
     void Start() {
-        for (int s = 0; s < 5; s++) value[s] = random(0, 135);
+        for (int s = 0; s < 5; s++) note[s] = random(0, 30);
+        quantizer.Init();
+        quantizer.Configure(OC::Scales::GetScale(5), 0xffff); // Semi-tone
     }
 
     void Controller() {
@@ -18,7 +24,7 @@ public:
             if (step == 0) ClockOut(1);
         }
 
-        Out(0, Proportion(value[step], 135, HEMISPHERE_MAX_CV));
+        Out(0, quantizer.Lookup(note[step] + 48));
     }
 
     void View() {
@@ -35,15 +41,31 @@ public:
     }
 
     void OnEncoderMove(int direction) {
-        value[selected] = constrain(value[selected] += direction, -1, 135);
+        if (note[selected] + direction < 0) {
+            // If turning past zero, set the mute bit for this step
+            muted |= (0x01 << selected);
+        } else {
+            note[selected] = constrain(note[selected] += direction, 0, 30);
+            muted &= ~(0x01 << selected);
+        }
     }
 
     uint32_t OnDataRequest() {
         uint32_t data = 0;
+        for (int s = 0; s < 5; s++)
+        {
+            Pack(data, PackLocation {s * 5,5}, note[s]);
+        }
+        Pack(data, PackLocation{25,5}, muted);
         return data;
     }
 
     void OnDataReceive(uint32_t data) {
+        for (int s = 0; s < 5; s++)
+        {
+            note[s] = Unpack(data, PackLocation {s * 5,5});
+        }
+        muted = Unpack(data, PackLocation {25,5});
     }
 
 protected:
@@ -57,35 +79,43 @@ protected:
     }
     
 private:
+    braids::Quantizer quantizer;
     int selected = 0;
-    int value[5]; // Sequence value (-1 - 135, and -1 means Off)
+    char muted = 0; // Bitfield for muted steps; ((muted >> step) & 1) means muted
+    int note[5]; // Sequence value (0 - 30)
     int step = 0; // Current sequencer step
 
     void Advance(int starting_point) {
         if (++step == 5) step = 0;
-        // If all the steps have been skipped, stay where we were
-        if (value[step] < 0 && step != starting_point) Advance(starting_point);
+        // If all the steps have been muted, stay where we were
+        if (step_is_muted(step) && step != starting_point) Advance(starting_point);
     }
 
     void DrawPanel() {
         // Sliders
         for (int s = 0; s < 5; s++)
         {
-            if (value[s] > -1) {
-                int x = 6 + (12 * s);
+            int x = 6 + (12 * s);
+            if (!step_is_muted(s)) {
                 gfxLine(x, 25, x, 63);
-                int p = Proportion(value[s], 135, 30) + 3;
 
                 // When selected, there's a heavier bar and a solid slider
                 if (s == selected) {
                     gfxLine(x + 1, 25, x + 1, 63);
-                    gfxRect(x - 4, BottomAlign(p), 9, 3);
-                } else gfxFrame(x - 4, BottomAlign(p), 9, 3);
+                    gfxRect(x - 4, BottomAlign(note[s]), 9, 3);
+                } else gfxFrame(x - 4, BottomAlign(note[s]), 9, 3);
 
                 // When on this step, there's an indicator circle
                 if (s == step) {gfxCircle(x, 20, 3);}
-            }
+            } else if (s == selected) {
+                gfxLine(x, 25, x, 63);
+                gfxLine(x + 1, 25, x + 1, 63);
+             }
         }
+    }
+
+    bool step_is_muted(int step) {
+        return (muted & (0x01 << step));
     }
 };
 
