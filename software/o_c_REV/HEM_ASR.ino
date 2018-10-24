2c90ffb36e813391e9505ee85365183a092556be
Jason Justian <jjustian@gmail.com>
2018-10-24 08:30:56 -0400

    ASR: comment change

--- software/o_c_REV/HEM_ASR.ino
+++ software/o_c_REV/HEM_ASR.ino
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include "HSRingBufferManager.h" // Singleton Lorenz manager
+#include "HSRingBufferManager.h" // Singleton Ring Buffer manager
 
 class ASR : public HemisphereApplet {
 public:
