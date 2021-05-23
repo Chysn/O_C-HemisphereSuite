db3867cf5f25ef706fcff19838707406fd0e27dc
Bryan Head <bryan.head@gmail.com>
2021-05-23 06:05:47 -0700

    Save master clock state

--- software/o_c_REV/APP_HEMISPHERE.ino
+++ software/o_c_REV/APP_HEMISPHERE.ino
@@ -60,6 +60,7 @@ enum HEMISPHERE_SETTINGS {
     HEMISPHERE_RIGHT_DATA_L,
     HEMISPHERE_LEFT_DATA_H,
     HEMISPHERE_RIGHT_DATA_H,
+    HEMISPHERE_CLOCK_DATA,
     HEMISPHERE_SETTING_LAST
 };
 
@@ -92,6 +93,7 @@ public:
             uint32_t data = (values_[4 + h] << 16) + values_[2 + h];
             available_applets[index].OnDataReceive(h, data);
         }
+        ClockSetup.OnDataReceive(0, uint32_t(values_[HEMISPHERE_CLOCK_DATA]));
     }
 
     void SetApplet(int hemisphere, int index) {
@@ -243,6 +245,7 @@ public:
             apply_value(2 + h, data & 0xffff);
             apply_value(4 + h, (data >> 16) & 0xffff);
         }
+        apply_value(HEMISPHERE_CLOCK_DATA, ClockSetup.OnDataRequest(0));
     }
 
     void OnSendSysEx() {
@@ -250,7 +253,7 @@ public:
         RequestAppletData();
 
         // Describe the data structure for the audience
-        uint8_t V[10];
+        uint8_t V[12];
         V[0] = (uint8_t)values_[HEMISPHERE_SELECTED_LEFT_ID];
         V[1] = (uint8_t)values_[HEMISPHERE_SELECTED_RIGHT_ID];
         V[2] = (uint8_t)(values_[HEMISPHERE_LEFT_DATA_L] & 0xff);
@@ -261,16 +264,18 @@ public:
         V[7] = (uint8_t)((values_[HEMISPHERE_LEFT_DATA_H] >> 8) & 0xff);
         V[8] = (uint8_t)(values_[HEMISPHERE_RIGHT_DATA_H] & 0xff);
         V[9] = (uint8_t)((values_[HEMISPHERE_RIGHT_DATA_H] >> 8) & 0xff);
+        V[10] = (uint8_t)(values_[HEMISPHERE_CLOCK_DATA] & 0xff);
+        V[11] = (uint8_t)((values_[HEMISPHERE_CLOCK_DATA] >> 8) & 0xff);
 
         // Pack it up, ship it out
         UnpackedData unpacked;
-        unpacked.set_data(10, V);
+        unpacked.set_data(12, V);
         PackedData packed = unpacked.pack();
         SendSysEx(packed, 'H');
     }
 
     void OnReceiveSysEx() {
-        uint8_t V[10];
+        uint8_t V[12];
         if (ExtractSysExData(V, 'H')) {
             values_[HEMISPHERE_SELECTED_LEFT_ID] = V[0];
             values_[HEMISPHERE_SELECTED_RIGHT_ID] = V[1];
@@ -278,6 +283,7 @@ public:
             values_[HEMISPHERE_RIGHT_DATA_L] = ((uint16_t)V[5] << 8) + V[4];
             values_[HEMISPHERE_LEFT_DATA_H] = ((uint16_t)V[7] << 8) + V[6];
             values_[HEMISPHERE_RIGHT_DATA_H] = ((uint16_t)V[9] << 8) + V[8];
+            values_[HEMISPHERE_CLOCK_DATA] = ((uint16_t)V[11] << 8) + V[10];
             Resume();
         }
     }
@@ -331,6 +337,7 @@ SETTINGS_DECLARE(HemisphereManager, HEMISPHERE_SETTING_LAST) {
     {0, 0, 65535, "Data R low", NULL, settings::STORAGE_TYPE_U16},
     {0, 0, 65535, "Data L high", NULL, settings::STORAGE_TYPE_U16},
     {0, 0, 65535, "Data R high", NULL, settings::STORAGE_TYPE_U16},
+    {0, 0, 65535, "Clock data", NULL, settings::STORAGE_TYPE_U16},
 };
 
 HemisphereManager manager;
